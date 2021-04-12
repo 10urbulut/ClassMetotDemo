eb6f001e45a084f9cef5f9d72a4852091941c7fb
10urb <10urbulut@gmail.com>
2021-04-12 17:48:44 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle()
+        {
+            Console.WriteLine("Müşteri eklendi!");
+        }
+
+        public void MusteriSil()
+        {
+            Console.WriteLine("Müşteri silindi!");
+        }
+        public void MusterileriListele()
+        {
+            Console.WriteLine("Müşteriler listelendi!");
+        }
+    }
+}
